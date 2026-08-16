7b68c6af4d47474b5b49588ae9009deae9f8e288
emilepetrin2-cell <emilepetrin2@gmail.com>
2026-08-15 23:30:29 -0400

    Time: 127 ms (12.44%) | Memory: 134.3 MB (5.48%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.cpp
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -7,15 +7,17 @@ public:
         std::unordered_set<int> fenetre;
 
         for (int i = 0; i < nums.size(); i++) {
-  
+            // 1. Si la fenêtre dépasse la taille k, on retire le plus vieux
             if (i > k) {
                 fenetre.erase(nums[i - k - 1]);
             }
 
+            // 2. Si le nombre est déjà présent dans les k dernières cases
             if (fenetre.find(nums[i]) != fenetre.end()) {
                 return true;
             }
 
+            // 3. On ajoute le nombre actuel
             fenetre.insert(nums[i]);
         }
 
